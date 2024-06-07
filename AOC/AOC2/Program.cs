cff2b01e5e30411ca03b319fecab808a3cef3070
Line Kargaard <lika62310@edu.ucl.dk>
2024-06-07 09:50:19 +0200

    AOC 2.2

--- AOC/AOC2/Program.cs
+++ AOC/AOC2/Program.cs
@@ -5,29 +5,31 @@
         static void Main(string[] args)
         {
             List<string> list = new List<string>();
+            List<int> min = new List<int>();
             StreamReader sr = new StreamReader("input.txt");
             string line = sr.ReadLine();
             while (line != null)
             {
                 list.Add(line);
                 string[] words = line.Split(' ');
+                int blue = 0;
+                int green = 0;
+                int red = 0;
                 for (int i = 0; i < words.Length; i++)
                 {
-                    if (i > 2 && words[i][0] == 'b' && int.Parse(words[i - 1]) > 14) list.Remove(line);
-                    if (i > 2 && words[i][0] == 'g' && int.Parse(words[i - 1]) > 13) list.Remove(line);
-                    if (i > 2 && words[i][0] == 'r' && int.Parse(words[i - 1]) > 12) list.Remove(line);
+                    if (i > 2 && words[i][0] == 'b' && int.Parse(words[i - 1]) > blue) blue = int.Parse(words[i - 1]);
+                    if (i > 2 && words[i][0] == 'g' && int.Parse(words[i - 1]) > green) green = int.Parse(words[i - 1]);
+                    if (i > 2 && words[i][0] == 'r' && int.Parse(words[i - 1]) > red) red = int.Parse(words[i - 1]);
                 }
+                min.Add(red * green * blue);
                 line = sr.ReadLine();
             }
             sr.Close();
 
             int total = 0;
-            foreach (string lines in list)
+            foreach(int num in min)
             {
-                Console.WriteLine(lines);
-                char[] delim = { ' ', ':' };
-                string[] words = lines.Split(delim);
-                total += int.Parse(words[1]);
+                total += num;
             }
             Console.WriteLine(total);
         }
