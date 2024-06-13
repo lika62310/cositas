7ea14148f0ce356bf0a173b5e792a9b7db688eed
Line Kargaard <lika62310@edu.ucl.dk>
2024-06-13 10:00:01 +0200

    AOC1

---
+++ AOC15/AOC15/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AOC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string line = sr.ReadLine();
+            int floor = 0;
+            while (line != null)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == '(') floor++;
+                    else if (line[i] ==  ')') floor--;
+                    if (floor == -1)
+                    {
+                        Console.WriteLine(i+1);
+                        break;
+                    }
+                }
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            Console.WriteLine(floor);
+        }
+    }
+}
